5da325a1f40ae0e8d453132a2ec46bb691bb97a3
nomadgeek <5652065+nomad-geek@users.noreply.github.com>
2023-11-01 21:39:50 -0400

    initial build

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+# set alpine as the base image of the Dockerfile
+FROM python:3.9-slim
+WORKDIR /app
+# Env var which will be the lower-level user
+ENV USER bot
+ENV GROUP_NAME group
+ENV OWNER user@example.com
+ENV SERVER_URL https://localhost
+ENV ACCESS_TOKEN SECRET_KEY
+ENV YT_URL https://www.youtube.com/feeds/videos.xml?channel_id=0000
+ENV POD_URL https://localhost
+ENV DELAY 10
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# Do all of this work as ROOT
+USER root
+
+# Create User
+RUN adduser ${USER} --system --no-create-home
+
+# update the package repository and install packages
+#RUN apk update && apk add py3-pip
+
+# Install Mastodon API
+RUN pip3 install Mastodon.py
+RUN pip3 install feedparser
+
+# Copy in the startup script & bot script
+COPY bot.py .
+
+# Set user during the container runtime
+USER ${USER}
+# Let's go!
+CMD [ "python", "/app/bot.py" ]
---
+++ bot.py
@@ -0,0 +1,85 @@
+import signal
+import time
+import logging, sys
+import os
+from mastodon import Mastodon
+import feedparser
+
+class GracefulKiller:
+    kill_now = False
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self,signum, frame):
+        self.kill_now = True
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    killer = GracefulKiller()
+
+    # Env Vars
+    SERVER_URL = os.getenv('SERVER_URL')
+    ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+    YT_URL = os.getenv('YT_URL')
+    POD_URL = os.getenv('POD_URL')
+    DELAY = int(os.getenv('DELAY')) * 60
+    GROUP_NAME = os.getenv('GROUP_NAME')
+    OWNER = os.getenv('OWNER')
+    # initialize Client
+    mastodon = Mastodon(api_base_url = SERVER_URL, access_token = ACCESS_TOKEN)
+
+    # init ID vars
+    currentYT = ''
+    currentPD = ''
+
+    # Get initial IDs so we know when there's a new one
+    logging.info("Initializing YouTube Feed..")
+    x = feedparser.parse(YT_URL)
+    currentYT = x.entries[0].id
+    logging.info("  Current Video ID is " + currentYT)
+
+    logging.info("Initializing Podcast Feed..")
+    x = feedparser.parse(POD_URL)
+    currentPD = x.entries[0].id
+    logging.info("  Current Pod ID is " + currentPD)
+
+    # Messaging
+    messageYT = "A new " + GROUP_NAME + " video has been posted!"
+    messagePD = "A new " + GROUP_NAME + " podcast has been posted!"
+
+    # Check in with owner
+    logging.info("Checking in with owner..")
+    mastodon.status_post("I'm online @" + OWNER, visibility='direct')
+
+    logging.info("Starting application loop..")
+    while True:
+        # Check if we should quit
+        if killer.kill_now:
+            break
+        # Check for new videos
+        logging.info("Checking for new Videos..")
+        x = feedparser.parse(YT_URL)
+        y = x.entries[0].id
+        z = x.entries[0].link
+        if y != currentYT:
+            currentYT = y
+            logging.info("Found new video!")
+            mastodon.status_post(messageYT + z)
+        # Check for new podcasts
+        logging.info("Checking for new Podcasts..")
+        x = feedparser.parse(POD_URL)
+        y = x.entries[0].id
+        z = x.entries[0].link
+        if y != currentPD:
+            currentPD = y
+            logging.info("Found new podcast!")
+            mastodon.status_post(messagePD + z)
+        # Start the sleep loop
+        logging.info("Checking Loop Complete. Zzzz..")
+        for number in range(DELAY):
+            time.sleep(1)
+            if killer.kill_now:
+                break
+                
+    logging.info("End of the program. I was killed gracefully :)")
